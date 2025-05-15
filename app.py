a840ae51d8ea43e71f1c5fb63b4c1adb1d29716d
bksregmi <bksregmi@gmail.com>
2025-05-15 09:45:38 +0545

    two buttons added in HTML

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, redirect, url_for,jsonify
+from flask import Flask, render_template, redirect, url_for,jsonify,request
 import mysql.connector
 
 app=Flask(__name__)
 
 db_config = {'host':'sql12.freesqldatabase.com','user':'sql12777796','password':'SktVxwR9Hp','database':'sql12777796'}
 
-def get_status():
+def get_status(varname):
     # Connect to the database
     conn = mysql.connector.connect(**db_config)
     cursor = conn.cursor()
-    cursor.execute("SELECT Value FROM Status WHERE Varname='status'")
+    cursor.execute("SELECT Value FROM Status WHERE Varname=%s",(varname,))
     status = cursor.fetchone()[0]  # Fetch the result
     conn.close()
     return status
@@ -20,26 +20,39 @@ def index():
     cursor = conn.cursor()
     cursor.execute("SELECT Value FROM Status WHERE Varname='status'")
     status = cursor.fetchone()[0]
+    
+    cursor.execute("SELECT Value FROM Status WHERE Varname='Emstat'")
+    emergency_status = cursor.fetchone()[0]
     conn.close()
-    return render_template('index.html', status=status)
+    return render_template('index.html', status=status, Emstat=emergency_status)
 
 
 @app.route('/toggle')
 def toggle():
+    button_type = request.args.get('type')
+    
+    
+    if button_type == 'normal':
+        varname = 'status'
+    elif button_type == 'emergency':
+        varname = 'Emstat'
+    else:
+        return redirect(url_for('index'))
     conn = mysql.connector.connect(**db_config)
     cursor = conn.cursor()
-    cursor.execute("SELECT Value FROM Status WHERE Varname='status'")
+    cursor.execute("SELECT Value FROM Status WHERE Varname=%s",(varname,))
     current_status = cursor.fetchone()[0]
     new_status = 0 if current_status == 1 else 1
-    cursor.execute("UPDATE Status SET Value=%s WHERE Varname='status'", (new_status,))
+    cursor.execute("UPDATE Status SET Value=%s WHERE Varname=%s", (new_status,varname))
     conn.commit()
     conn.close()
     return redirect(url_for('index'))
 
 @app.route("/status", methods=["GET"])
 def get_status_endpoint():
-    status = get_status()  # Fetch the current status from DB
-    return jsonify({"status": status})  # Send it as a JSON response
+    status = get_status('status')  # Fetch the current status from DB
+    emstat = get_status('Emstat')
+    return jsonify({"status": status,"Emstat":emstat})  # Send it as a JSON response
 
             
 if __name__ == '__main__':
