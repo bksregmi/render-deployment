b67c2dd1d3f7c2667ede0c382a3fbffab93f28eb
bksregmi <bksregmi@gmail.com>
2025-05-18 16:23:13 +0545

    Changed Database to MongoDB

--- app.py
+++ app.py
@@ -1,57 +1,55 @@
 from flask import Flask, render_template, redirect, url_for,jsonify,request
+from pymongo import MongoClient
 import mysql.connector
 
 app=Flask(__name__)
 
-db_config = {'host':'sql12.freesqldatabase.com','user':'sql12779194','password':'Q2z6DmdZZD','database':'sql12779194'}
+client=MongoClient("mongodb+srv://workingyatri:QF0jmjKOfREtICoS@cluster1.fqv7v2j.mongodb.net/")
+db=client['Status_Table']
+collection=db['Status']
+STATUS_DOC_ID = 1
 
-def get_status(varname):
+def get_status():
     # Connect to the database
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor()
-    cursor.execute("SELECT Value FROM Status WHERE Varname=%s",(varname,))
-    status = cursor.fetchone()[0]  # Fetch the result
-    conn.close()
-    return status
+    doc=collection.find_one({"_id":STATUS_DOC_ID})
+    if doc:
+        return doc.get("status",0),doc.get("Emstat",0)
+    else:
+        return -1,-1
 
 @app.route('/')
 def index():
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor()
-    cursor.execute("SELECT Value FROM Status WHERE Varname='status'")
-    status = cursor.fetchone()[0]
-    
-    cursor.execute("SELECT Value FROM Status WHERE Varname='Emstat'")
-    emergency_status = cursor.fetchone()[0]
-    conn.close()
-    return render_template('index.html', status=status, Emstat=emergency_status)
+    status,emstat=get_status()
+    return render_template('index.html', status=status, Emstat=emstat)
 
 
 @app.route('/toggle')
 def toggle():
     button_type = request.args.get('type')
-    
+    update_field=None
     
     if button_type == 'normal':
-        varname = 'status'
+        update_field = 'status'
     elif button_type == 'emergency':
-        varname = 'Emstat'
+        update_field = 'Emstat'
     else:
         return redirect(url_for('index'))
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor()
-    cursor.execute("SELECT Value FROM Status WHERE Varname=%s",(varname,))
-    current_status = cursor.fetchone()[0]
-    new_status = 0 if current_status == 1 else 1
-    cursor.execute("UPDATE Status SET Value=%s WHERE Varname=%s", (new_status,varname))
-    conn.commit()
-    conn.close()
+    doc=collection.find_one({"_id":STATUS_DOC_ID})
+    if not doc:
+        doc={"_id":STATUS_DOC_ID,"status":-1,"Emstat":-1}
+        collection.insert_one(doc)
+        
+    current_value=doc.get(update_field,0)
+    new_value=0 if current_value==1 else 1
+    collection.update_one(
+        {"_id": STATUS_DOC_ID},
+        {"$set": {update_field: new_value}}
+    )    
     return redirect(url_for('index'))
 
 @app.route("/status", methods=["GET"])
 def get_status_endpoint():
-    status = get_status('status')  # Fetch the current status from DB
-    emstat = get_status('Emstat')
+    status,emstat=get_status()
     return jsonify({"status": status,"Emstat":emstat})  # Send it as a JSON response
 
             
---
+++ mongo_try.py
@@ -0,0 +1,12 @@
+import pymongo
+
+if __name__=="__main__":
+    client=pymongo.MongoClient("mongodb+srv://workingyatri:QF0jmjKOfREtICoS@cluster1.fqv7v2j.mongodb.net/")
+    print(client)
+    db=client['Status_Table']
+    collection=db['Status']
+    one=collection.find_one({'_id':1},{'status':1,'Emstat':1,'_id':0})
+    print(one)
+    prev={"status":0}
+    next={"$set":{"status":1}}
+    collection.update_one(prev,next)
